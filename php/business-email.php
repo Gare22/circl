9d891c61403a91ec9f810d3db743cf2b1140fd92
Gare22 <Garesgaming@gmail.com>
2021-11-16 16:29:50 -0500

    POST request for contact and business forms started

---
+++ php/business-email.php
@@ -0,0 +1,17 @@
+<?php
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $phone = $_POST['phone'];
+    $useremail = $_POST['email'];
+    $business = $_POST['business'];
+
+    $formemail = "GTallent@cascadespares.com";
+
+    echo($firstname . "<br>" . $lastname . "<br>" . $phone . "<br>" . $email . "<br>" . $business);
+
+
+    //login to email
+
+    //send email to self with reply-to the person's email
+
+?>
