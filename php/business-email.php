acb88eed9973d34a85c2a6b1970bca94dc325895
Gare22 <Garesgaming@gmail.com>
2021-12-14 10:28:10 -0500

    Composer and PHP Mailer added to branch

--- php/business-email.php
+++ php/business-email.php
@@ -1,17 +1,24 @@
-<?php
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $phone = $_POST['phone'];
-    $useremail = $_POST['email'];
-    $business = $_POST['business'];
+<?php 
 
-    $formemail = "GTallent@cascadespares.com";
+if(isset($_POST['submit'])){
 
-    echo($firstname . "<br>" . $lastname . "<br>" . $phone . "<br>" . $useremail . "<br>" . $business);
+    $mailserver = '{imap-mail.outlook.com:993/ssl/novalidate-cert}';
+    $to = "example@email.com"; // this is your Email address
+    $password = "password";
+    
+    
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    echo "<script>console.log('".$first_name."');</script>"; //debug console log
+    
+    $subject = "Form submission";//Subject line
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['business']; //actual message
 
-
-    //login to email
-
-    //send email to self with reply-to the person's email
-
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
