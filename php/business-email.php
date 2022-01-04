c58cab982f296bbc4a98aca6fbc466fc2e27d3f2
Gare22 <Garesgaming@gmail.com>
2022-01-04 16:02:55 -0500

    Added and removed some comments.

--- php/business-email.php
+++ php/business-email.php
@@ -2,6 +2,7 @@
 //business-email.php
 //description: Obtains business email form information via post HTTP method then sends 
 //     email with PHPMailer (https://github.com/PHPMailer/PHPMailer) via gmail account to Dawson
+//author: Garrett Tallent (https://github.com/gare22)
 
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
@@ -27,16 +28,16 @@ if(isset($_POST['submit'])){
     //Code pulled from PHP Mailer readme.md (https://github.com/PHPMailer/PHPMailer)
     try {
         //Server settings
-        $mail->SMTPDebug = 2;//Enable verbose debug output
-        $mail->isSMTP();//Send using SMTP
-        $mail->SMTPAuth = true;//Enable SMTP authentication
-        $mail->SMTPSecure = 'tls';//Enable implicit TLS encryption
-        $mail->Port = 587;//TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-        $mail->Host = 'smtp.gmail.com';//Set the SMTP server to send through
+        $mail->SMTPDebug = 2;
+        $mail->isSMTP();
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+        $mail->Host = 'smtp.gmail.com';
 
         //Email address and password
-        $mail->Username = 'fake@fake.com';//email address you'll be sending from
-        $mail->Password = 'fakepassword';//password of above email address
+        $mail->Username = 'fake@fake.com';
+        $mail->Password = 'fakepassword';
         
     
         //Recipients
@@ -44,18 +45,9 @@ if(isset($_POST['submit'])){
         $mail->addAddress('support@circl.com', 'Circl - Support');//Address of where we want to receive our contact forms
         $mail->addReplyTo($email, $business);
         
-        //$mail->addCC('cc@example.com');
-        //$mail->addBCC('bcc@example.com');
-    
-        
-        /* (Commented out in case we want to use later) - Gare
-        //Attachments
-        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-        */
 
         //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->isHTML(true);//Set email format to HTML
         $mail->Subject = $business . ' would like to sign up';
         $mail->Body    = '<b>' . $business . '</b> would like to sign up. <br>Contact them through ' . $first_name .' '. $last_name .' at ' . $email . ' or phone number ' . $phone . '.';
         $mail->AltBody = $business . ' would like to sign up. Contact them through ' . $first_name .' '. $last_name .' at ' . $email . ' or phone number ' . $phone . '.';
--- php/feedback-email.php
+++ php/feedback-email.php
@@ -2,6 +2,7 @@
 //feedback-email.php
 //description: Obtains contact us form information via post HTTP method then sends 
 //     email with PHPMailer (https://github.com/PHPMailer/PHPMailer) via gmail account to Dawson
+//author: Garrett Tallent (https://github.com/gare22)
 
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
@@ -27,16 +28,16 @@ if(isset($_POST['submit'])){
     //Code copied from PHP Mailer readme.md
     try {
         //Server settings
-        $mail->SMTPDebug = 2;//Enable verbose debug output
-        $mail->isSMTP();//Send using SMTP
-        $mail->SMTPAuth = true;//Enable SMTP authentication
-        $mail->SMTPSecure = 'tls';//Enable implicit TLS encryption
-        $mail->Port = 587;//TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-        $mail->Host = 'smtp.gmail.com';//Set the SMTP server to send through
+        $mail->SMTPDebug = 2;
+        $mail->isSMTP();
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+        $mail->Host = 'smtp.gmail.com';
 
         //Email address and password
-        $mail->Username = 'Fake@gmail.com';//email address you'll be sending from
-        $mail->Password = 'Fake';//password of above email address
+        $mail->Username = 'Fake@gmail.com';
+        $mail->Password = 'Fake';
         
     
         //Recipients
@@ -44,18 +45,9 @@ if(isset($_POST['submit'])){
         $mail->addAddress('Fake@Fake.com', 'Fake');//Address of where we want to receive our contact forms
         $mail->addReplyTo($email, $first_name);
         
-        //$mail->addCC('cc@example.com');
-        //$mail->addBCC('bcc@example.com');
-    
-        
-        /* (Commented out in case we want to use later) - Gare
-        //Attachments
-        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-        */
 
         //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->isHTML(true);//Set email format to HTML
         $mail->Subject = '' . $first_name . ' ' . $last_name . ' has feedback for us.';
         $mail->Body    = '<b>' . $first_name . ' ' . $last_name . '</b> has given us feedback. <br>Contact them at ' . $email . ' or phone number ' . $phone . '.<br> Their feedback says: <br><b>' . $message . '</b>';
         $mail->AltBody = $first_name . ' ' . $last_name . ' has given us feedback. Contact them at ' . $email . ' or phone number ' . $phone . '. Their feedback says: ' . $message;
